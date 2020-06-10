fd353db053643599395b0a673df0a81f1de710a5
nafsadh <khan@nafsadh.com>
2020-06-10 00:42:32 -0700

    [app] rename main.scss to style.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['@/assets/scss/main.scss'],
+  css: ['@/assets/scss/style.scss'],
   /*
    ** Plugins to load before mounting the App
    */
