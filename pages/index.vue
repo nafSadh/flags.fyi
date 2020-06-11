dbe8f4cb2270652068d6c21204fcc4d5084981b2
nafsadh <khan@nafsadh.com>
2020-06-10 22:02:00 -0700

    [app] create proper landing page wth logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on GitHub:
-        <a href="https://github.com/nafSadh/flags.fyi">
-          nafSadh/flags.fyi
-        </a>
-      </card>
+  <section class="hero is-fullheight has-background-light">
+    <div class="hero-body ">
+      <div class="container has-text-centered">
+        <figure class="image box px-0 py-0 container is-256x256">
+          <img src="~/assets/logo/flagstaff-grid.svg" />
+        </figure>
+        <p class="title">
+          Flags.fyi
+        </p>
+        <button class="button is-primary">
+          <nuxt-link class="has-text-white" to="/flagIndex">
+            <span>Goto: Index of Flags</span>
+          </nuxt-link>
+        </button>
+      </div>
     </div>
   </section>
 </template>
-
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'HomePage',
-
-  components: {
-    Card
-  }
+  layout: 'vanilla',
+  components: {}
 }
 </script>
