1a1fab9042fce3a65a13ee4cfa8c24c0fe0cbbf0
nafsadh <khan@nafsadh.com>
2020-06-10 22:26:55 -0700

    [app] move nav-bar code to a component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,16 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-light"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item has-text-primary" href="/">
-            <img
-              src="~assets/logo/flagstaff-grid-sm.svg"
-              alt="flags.fyi"
-              height="32"
-            />&nbsp;
-            <b class="">Flags.fyi</b>
-          </a>
-          <nuxt-link class="navbar-item is-hidden-desktop" to="/flagIndex">
-            <span class="icon">
-              <fa icon="list-ul" />
-            </span>
-          </nuxt-link>
-          <div class="navbar-burger">
-            <span />
-            <span />
-            <span />
-          </div>
-        </div>
-        <div class="navbar-menu">
-          <div class="navbar-end">
-            <nuxt-link class="navbar-item " to="/flagIndex">
-              <span class="icon">
-                <fa icon="list-ul" />
-              </span>
-              <span>Index of Flags</span>
-            </nuxt-link>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <nav-bar />
     <nuxt />
   </div>
 </template>
 
 <script>
+import NavBar from '~/components/NavBar'
 export default {
+  components: {
+    NavBar
+  },
   data() {
     return {}
   },
