d2fb036f49c5ce5a5531d556ca3178350bb4c429
nafsadh <khan@nafsadh.com>
2020-06-10 21:53:54 -0700

    [app] add an empty layout

---
+++ layouts/empty.vue
@@ -0,0 +1,15 @@
+<template>
+  <nuxt />
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  head: {
+    bodyAttrs: {
+      class: 'has-background-white'
+    }
+  }
+}
+</script>
