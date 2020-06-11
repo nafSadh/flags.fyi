f23b560af7c40f26f780ff336405ac9284a72b63
nafsadh <khan@nafsadh.com>
2020-06-10 23:39:27 -0700

    [app] display flags inside an imposing hero

--- pages/_flagId.vue
+++ pages/_flagId.vue
@@ -1,16 +1,20 @@
 <template>
-  <section class="section main-content container">
-    <div v-if="hasFlagSvg" class="box is-radiusless	has-background-light">
-      <div class="card-image">
-        <figure class="image">
-          <img :src="require('~/assets/export/' + `${flagSvg}`)" />
-        </figure>
+  <div>
+    <section v-if="hasFlagSvg" class="hero is-light is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <figure class="image box px-0 py-0 container">
+            <img :src="require('~/assets/export/' + `${flagSvg}`)" />
+          </figure>
+        </div>
       </div>
-    </div>
-    <article class="content">
-      <h1 class="title ">{{ title }}</h1>
-    </article>
-  </section>
+    </section>
+    <section class="section main-content container">
+      <article class="content">
+        <h1 class="title ">{{ title }}</h1>
+      </article>
+    </section>
+  </div>
 </template>
 <script>
 import flagJson from '~/static/flags.json'
