ccc65cf10c15bb579bd06fbea18ad24a5ac08ac2
nafsadh <khan@nafsadh.com>
2020-06-21 15:26:03 -0700

    [app] add nuxt-link-button component

---
+++ components/NuxtLinkButton.vue
@@ -0,0 +1,19 @@
+<!-- a button in a <nuxt-link/>, can have fa icon and the slot. -->
+<template>
+  <nuxt-link :to="to" class="button">
+    <span v-if="!!icon" class="icon is-small">
+      <fa :icon="icon" />
+    </span>
+    <span v-if="this.$slots.default">
+      <slot></slot>
+    </span>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  props: {
+    to: { type: Object, required: true },
+    icon: { type: String, default: undefined }
+  }
+}
+</script>
