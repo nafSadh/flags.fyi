6f3a8fbd2d51e940603067f3af753357815d16d9
nafsadh <khan@nafsadh.com>
2020-07-01 19:33:08 -0700

    [app] check if meta entry exists in BrowseFlagsButton instead of facing error

--- components/BrowseFlagsButtons.vue
+++ components/BrowseFlagsButtons.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="buttons has-addons">
     <nuxt-link-button
+      v-if="toPrev"
       v-shortkey="{ 0: ['arrowleft'], p: ['p'] }"
       icon="chevron-left"
       :to="toPrev"
       @shortkey.native="$router.push(toPrev)"
     />
     <nuxt-link-button
+      v-if="toNext"
       v-shortkey="{ 0: ['arrowright'], n: ['n'] }"
       icon="chevron-right"
       :to="toNext"
@@ -23,8 +25,12 @@ export default {
   props: { flagId: { type: String, required: true } },
   data() {
     return {
-      toPrev: this.$toFlagId(metadata[this.flagId].prev),
-      toNext: this.$toFlagId(metadata[this.flagId].next)
+      toPrev: metadata[this.flagId]
+        ? this.$toFlagId(metadata[this.flagId].prev)
+        : undefined,
+      toNext: metadata[this.flagId]
+        ? this.$toFlagId(metadata[this.flagId].next)
+        : undefined
     }
   }
 }
